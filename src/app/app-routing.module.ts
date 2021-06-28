d0b10ee2b1cc3a5b3b713de140468393662e30ee
Ilya-Marvin <ilya44.m@gmail.com>
2021-06-28 12:11:47 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BreweriesComponent } from './breweries/breweries.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : '', component: HomeComponent},
+  {path: 'breweries', component: BreweriesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
